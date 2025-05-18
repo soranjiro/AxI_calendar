e5692a4ad8b4583b1b8738a60675af0000f7deaa
ryosuke0828 <ryosukee.zzz@gmail.com>
2025-05-18 19:36:12 +0900

    feat: Add featureRegistry to UseCase and update NewUseCase signature

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -2,19 +2,22 @@ package usecase
 
 import (
 	dynamodbrepo "github.com/soranjiro/axicalendar/internal/adapter/persistence/dynamodb"
+	"github.com/soranjiro/axicalendar/internal/domain/feature" // feature をインポート
 )
 
 // UseCase implements the UseCaseInterface.
 type UseCase struct {
-	themeRepo dynamodbrepo.ThemeRepository
-	entryRepo dynamodbrepo.EntryRepository
+	themeRepo       dynamodbrepo.ThemeRepository
+	entryRepo       dynamodbrepo.EntryRepository
+	featureRegistry feature.ExecutorRegistry // featureRegistry を追加
 	// Add other repositories or services as needed
 }
 
 // NewUseCase creates a new UseCase with dependencies.
-func NewUseCase(themeRepo dynamodbrepo.ThemeRepository, entryRepo dynamodbrepo.EntryRepository) *UseCase {
+func NewUseCase(themeRepo dynamodbrepo.ThemeRepository, entryRepo dynamodbrepo.EntryRepository, featureRegistry feature.ExecutorRegistry) *UseCase { // featureRegistry を引数に追加
 	return &UseCase{
-		themeRepo: themeRepo,
-		entryRepo: entryRepo,
+		themeRepo:       themeRepo,
+		entryRepo:       entryRepo,
+		featureRegistry: featureRegistry, // featureRegistry を設定
 	}
 }
