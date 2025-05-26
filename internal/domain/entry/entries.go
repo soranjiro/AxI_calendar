25e50ab0c7ddc533af198527bd6f3ff4f0cad343
ryosuke0828 <ryosukee.zzz@gmail.com>
2025-05-26 22:51:17 +0900

    entriesに対するメソッドの実装

--- internal/domain/entry/entries.go
+++ internal/domain/entry/entries.go
@@ -1,14 +1,29 @@
 package entry
 
 import (
-	"context"
 	"fmt"
-	"time"
-
-	"github.com/soranjiro/axicalendar/internal/domain/theme" // For validation
-
-	"github.com/google/uuid"
 )
-
 type Entries []Entry
+
+func (e Entries) SumAll() (float64, error) {
+	var sum float64
+	for _, entry := range e {
+		val, ok := entry.Data["amount"]
+		if !ok {
+			return 0, fmt.Errorf("amount field not found in entry data for entry ID %s", entry.EntryID)
+		}
+		switch v := val.(type) {
+		case float64:
+			sum += v
+		case int:
+			sum += float64(v)
+		case int32:
+			sum += float64(v)
+		case int64:
+			sum += float64(v)
+		default:
+		}
+	}
+	return sum, nil
+}
 
